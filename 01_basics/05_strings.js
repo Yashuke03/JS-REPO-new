d9f037c85024728eceb262db94fa30c6b3181f8a
yash uke <yashuke2006@gmail.com>
2026-02-18 17:42:16 +0000

    More About Strings and NUM with Math

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name = "js";
+const newCount = 38;
+
+// console.log(`Hello this is ${name} and total numbers are ${newCount}` );
+
+const game = new String('yashuke');
+
+// console.log(game[6]);
+// console.log(game.__proto__);
+
+// console.log(game.length);
+// console.log(game.toUpperCase());
+// console.log(game.charCodeAt(1));  //97
+
+// console.log(game.endsWith("k",6));
+
+// console.log(`Print my name if ${game.includes("uke")}`)
+
+const newString = game.substring(0,5);
+// console.log(newString);
+
+const newStr = game.slice(-2,7);
+// console.log(newStr);
+
+// console.log(game.split('h'));
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,43 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(256);
+// console.log(balance);
+// console.log(balance.toString().length);
+
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.NaN);
+// console.log(Number.toString());
+// console.log(parseInt("139"));
+
+const some = 3659.9882843;
+
+// console.log(some.toPrecision());
+// console.log(some.toFixed(1));
+
+const num = 1000000000
+// console.log(num.toLocaleString());
+
+//  ***************************  MATH ***************************
+
+// console.log(Math);
+// console.log(Math.abs(-38));
+// console.log(Math.round(294823.989898));
+// console.log(Math.ceil(294823.9898));
+// console.log(Math.floor(294823.0498));
+
+// console.log(Math.random());
+// console.log((Math.random()*100)+1);
+// console.log(Math.floor(Math.random()*100)+1);
+
+const min = 5;
+const max = 8;
+
+// console.log(Math.floor(Math.random() * (max - min +1) +min));
+
+// let hello = Math.random();
+// console.log(hello);
+// console.log(Math.floor(hello * (max - min )));
+
